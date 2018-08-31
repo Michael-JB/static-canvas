59f5d19619ebe10bd52bfa3777ae4a877d3d6505
Michael B <michael@ramyun.co.uk>
2018-08-31 17:00:50 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+var w = 600, h = 800, x = 0, y = 0;
+var resizeTimer;
+
+var greyscale = true;
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function generateStatic() {
+  setDimensions();
+  for (i = 0; i < w * h; i++) {
+    x = i % w;
+    y = Math.floor(i / w);
+  
+    ctx.fillStyle = 'rgb(' + generateRandomColour(greyscale).join(', ') + ')';
+    ctx.fillRect(x, y, 1, 1);
+  }
+}
+
+function generateRandomColour(greyscale) {
+  var col = [];
+  col[0] = getRandomInt(0, 255);
+
+  for (c = 1; c < 3; c++) {
+    if (greyscale) col[c] = col[0];
+    else col[c] = getRandomInt(0, 255);
+  }
+
+  return col;
+}
+
+function onResize() {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    generateStatic();
+  }, 250);
+}
+
+function setDimensions() {
+  w = document.documentElement.clientWidth;
+  h = document.documentElement.clientHeight;
+
+  canvas.width = w;
+  canvas.height = h;
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
