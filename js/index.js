7472cd3bc5dcba6fdf31db54d67fccef4fc596b9
Michael B <michael@ramyun.co.uk>
2018-09-03 14:42:57 +0100

    Switched to document ready

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var greyscale = false;
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-$(window).on("load", function() {
+$(document).ready(function() {
   showLoader();
   generateStatic();
   hideLoader();
